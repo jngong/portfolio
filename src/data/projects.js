aa0d5bea8e9d1af1f704ddd1fc22310f75d3f9f1
jngong <34215359+jngong@users.noreply.github.com>
2020-03-18 11:47:04 -0400

    added data file for projects

---
+++ src/data/projects.js
@@ -0,0 +1,47 @@
+export const projectData = [
+    {
+        title: "EntertainmentIQ",
+        tech_stack: "HTML, CSS, Javascript, Axios, Open Trivia DB API",
+        overview: "Test your knowledge in film, TV and music with this interactive one-player trivia game. You'll get 12 random questions to attempt. This web app was built in one week with HTML, CSS, vanilla Javascript and data from the Open Trivia Database API. ",
+        features: "Complex game logic with score tracking and reset capability, Local storage for unique session token, Responsive media queries, Keyframe animation",
+        image: "",
+        site_link: "https://pages.git.generalassemb.ly/jngong/entertainment-iq-trivia/",
+        github_link: ""
+    },
+    {
+        title: "Gender Diversity in Film",
+        tech_stack: "React, CSS, Flexbox, The Movie Database API, Bechdel Test API",
+        overview: "When it comes to gender parity in entertainment, Hollywood has a long way to go. Explore gender representation in the film industry by viewing gender breakdown percentages for the cast / crew and Bechdel Test results where available. This web app was built in one week to demonstrate full understanding of the React.js framework.",
+        features: "User search for film title, Default home page view of trending movies, Dynamic routing to film detail pages, Calculation of cast / crew gender %, Match data from 2 APIs based on common identifier, Conditional rendering of Bechdel Test results, Responsive media queries",
+        image: "",
+        site_link: "https://gender-diversity-in-film.surge.sh/",
+        github_link: ""
+    },
+    {
+        title: "Travelogue",
+        tech_stack: "React, CSS, Ruby on Rails, PostgreSQL, bcrypt, JWT, Heroku",
+        overview: "Ever been asked for travel recommendations and come up at a loss for what you enjoyed on that trip 3 years ago? Travelogue helps you keep track of where you've been so you can help others get to where they're going. This full-stack web app was built in one week as my final capstone project for my Software Engineering Immersive course.",
+        features: "User sign up / login with encrypted password & JWT authorization, User can create, read, update & delete their own entries, Public view of places filterable by country, Model-View-Controller (MVC) design pattern, REST API backend, Responsive media queries",
+        image: "",
+        site_link: "https://travelogue-app.herokuapp.com/",
+        github_link: ""
+    },
+    {
+        title: "Expedia",
+        tech_stack: "React, CSS, Node.js, Express.js, Sequelize, PostgreSQL, Heroku",
+        overview: "As consumers demand more sustainable options in their everyday lives, we've introduced a new feature for Expedia to highlight eco-friendly hotels. In collaboration with a team of UX designers and engineers, we built a full-stack Expedia clone in one week following agile / scrum methodology and using React, CSS, Node.js, Express.js, Sequelize and PostgreSQL.",
+        features: "Mobile first design, User search for bundled vacation packages, Checkbox for eco-friendly options only, User flow to select hotels and room types, Pixel perfect CSS matching UX mock-ups in Zeplin, Admin section for listing, updating or deleting a new hotel",
+        image: "",
+        site_link: "https://expedia-flying-four.herokuapp.com/",
+        github_link: ""
+    },
+    {
+        title: "Friends of Columbus Park",
+        tech_stack: "HTML, CSS, jQuery, Fancybox, Slick Carousel",
+        overview: "Redesigned website for a local Chinatown, NY based not-for-profit organization focused on protecting and advocating for Columbus Park and its surrounding community members. This was the first end-to-end project that I built as a developer and was the final project for my part-time Front End Web Development course.",
+        features: "Sticky navigation bar, Mobile responsiveness incl. collapsable menu, Integration of jQuery libraries for image carousel and lightbox functionality, Heavy use of flexbox for layout",
+        image: "",
+        site_link: "https://jngong.github.io/",
+        github_link: ""
+    }
+]
