a8c33b93b8c91e5e432cadef947c7a4f19aa395e
jngong <34215359+jngong@users.noreply.github.com>
2020-03-18 21:31:04 -0400

    added missing skills

--- src/data/skills.js
+++ src/data/skills.js
@@ -3,12 +3,14 @@ export const skillsData = [
     { name: "CSS3", icon: "devicon-css3-plain" },
     { name: "HTML5", icon: "devicon-html5-plain" },
     { name: "React", icon: "devicon-react-original" },
+    { name: "Ruby", icon: "devicon-ruby-plain" },
+    { name: "Rails", icon: "devicon-rails-plain" },
+    { name: "jQuery", icon: "devicon-jquery-plain" },
     { name: "Node.js", icon: "devicon-nodejs-plain" },
-    { name: "Ruby", icon: "devicon-ruby-plain"},
-    { name: "Rails", icon: "devicon-rails-plain"},
-    { name: "Sequelize", icon: "devicon-sequelize-plain"},
-    { name: "PostgreSQL", icon: "devicon-postgresql-plain"},
-    { name: "Git", icon: "devicon-git-plain"},
-    { name: "Github", icon: "devicon-github-plain"},    
-    { name: "Heroku", icon: "devicon-heroku-original"}    
+    { name: "Express.js", icon: "devicon-express-original" }, 
+    { name: "Sequelize", icon: "devicon-sequelize-plain" },
+    { name: "PostgreSQL", icon: "devicon-postgresql-plain" },
+    { name: "Git", icon: "devicon-git-plain" },
+    { name: "Github", icon: "devicon-github-plain" },
+    { name: "Heroku", icon: "devicon-heroku-original" }
 ]
