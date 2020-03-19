bc772878f6e7b994ae6ac7c446ddf36dcb44a5a3
jngong <34215359+jngong@users.noreply.github.com>
2020-03-19 17:21:38 -0400

    created form component

--- src/components/Layout/Contact.js
+++ src/components/Layout/Contact.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react'
 import ContactForm from './ContactForm'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-
 
 const Contact = () => {
 
-    const [formDetails, setFormDetails] = useState({ name: '', email: '', message: '' })
+    const [formDetails, setFormDetails] = useState({ name: '', _replyto: '', message: '' })
+    const [status, setStatus] = useState('')
 
     const handleChange = e => {
         const { name, value } = e.target
 
         setFormDetails(prevState => ({ ...prevState, [name]: value }))
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        const form = e.target
+        const data = new FormData(form)
+        const xhr = new XMLHttpRequest()
+        xhr.open(form.method, form.action)
+        xhr.setRequestHeader("Accept", "application/json")
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return
+            if (xhr.status === 200) {
+                form.reset()
+                setStatus("SUCCESS")
+            } else {
+                setStatus("ERROR")
+            }
+        }
+        xhr.send(data)
     }
 
     return (
@@ -20,11 +38,11 @@ const Contact = () => {
                 <div className='header-line'></div>
             </div>
             <div className='contact-description'>
-                <p>Thank you for taking the time to review my portfolio. I'm currently open to full-time, freelance and contract opportunities.</p>
-                <p> You can connect with me on <a href="https://www.linkedin.com/in/jngong/" target="blank">LinkedIn</a> or send me a message via the form below. Talk to you soon!
-            </p>
+                <p>
+                    Thank you for taking the time to review my portfolio. I'm currently open to full-time, freelance and contract opportunities. You can connect with me on <a href="https://www.linkedin.com/in/jngong/" target="blank">LinkedIn</a> or send me a message via the form below. Talk to you soon!
+                </p>
             </div>
-            <ContactForm formDetails={formDetails} handleChange={handleChange} />
+            <ContactForm formDetails={formDetails} handleChange={handleChange} handleSubmit={handleSubmit} status={status} />
         </div>
 
     )
--- src/components/Layout/ContactForm.js
+++ src/components/Layout/ContactForm.js
@@ -1,35 +1,58 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1)
+    },
+  },
+}));
+
 
 const ContactForm = (props) => {
-
+    const classes = useStyles();
     return(
-        <form>
-            <label>Full Name</label>
-            <input 
-                autoComplete='off'
-                type='text'
+        <form 
+            action='https://formspree.io/xdodleop'
+            method='POST'
+            className={classes.root} 
+            autoComplete='off'
+            onSubmit={props.handleSubmit}
+        >
+            <TextField 
+                id='filled-name'
                 name='name'
+                label='Name'
                 value={props.formDetails.name}
                 onChange={props.handleChange}
+                variant='filled'
             />
-            <label>Email Address</label>
-            <input 
-                autoComplete='off'
-                type='text'
-                name='email'
-                value={props.formDetails.email}
+            <TextField 
+                id='filled-email'
+                name='_replyto'
+                label='Email Address'
+                value={props.formDetails._replyto}
                 onChange={props.handleChange}
+                variant='filled'
             />
-            <label>Message</label>
-            <textarea 
-                autoComplete='off'
+            <TextField 
+                id='filled-message'
                 name='message'
+                label='Let me know what you are interested in speaking about.'
                 value={props.formDetails.message}
                 onChange={props.handleChange}
+                multiline
+                rows='3'
+                variant='filled'
             />
-            <button>
-                Send 
-            </button>
+            {props.status === 'SUCCESS' ? <p>Thanks! I'll be in touch soon</p> : 
+            <Button variant='contained' color='primary' size='medium' type='submit' value='Send'>
+                Send Message
+            </Button>}
+            {props.status === 'ERROR' && <p>'Sorry, there was an error. Please try again.'</p>}
         </form>
     )
 }
