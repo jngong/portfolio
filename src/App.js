cbec0301988c2608c5d05e9931d2c30586e2374f
jngong <34215359+jngong@users.noreply.github.com>
2020-03-19 18:22:39 -0400

    media queries for tablet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Layout from './components/Layout/Layout'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import './App.css';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#213248', contrastText: '#FFFFFF' },
+    secondary: { main: '#3DBDD7', contrastText: '#213248' }
+  }
+})
 
 function App() {
   return (
     <div>
+      <ThemeProvider theme={theme} >
         <Layout />
+      </ThemeProvider>
     </div>
   );
 }
