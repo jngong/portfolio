ef41faf62e92e050f080b5e03c2270f2e0293718
jngong <34215359+jngong@users.noreply.github.com>
2020-03-19 09:54:51 -0400

    edited project descriptions

--- src/components/Layout/Hero.js
+++ src/components/Layout/Hero.js
@@ -6,6 +6,7 @@ const Hero = () => {
             <h3>Hello!</h3>
             <h1>I'm Jennifer Gong</h1>
             <h3>Software Developer</h3>
+            <h4>New York, NY</h4>
         </div>
     )
 }
--- src/data/projects.js
+++ src/data/projects.js
@@ -7,8 +7,8 @@ import Travelogue from '../images/travelogue.jpeg'
 export const projectData = [
     {
         title: "EntertainmentIQ",
-        tech_stack: "HTML, CSS, Javascript, Axios, Open Trivia DB API",
-        overview: "Test your knowledge in film, TV and music with this interactive one-player trivia game. You'll get 12 random questions to attempt. This web app was built in one week with HTML, CSS, vanilla Javascript and data from the Open Trivia Database API. ",
+        tech_stack: "HTML, CSS, Javascript, Open Trivia DB API",
+        overview: "Test your knowledge in film, TV and music with this interactive one-player trivia game. You'll get 12 random questions and a final score based on how many you guess right. Are you the master of random entertainment knowledge? Prove it.",
         features: "Complex game logic with score tracking and reset capability, Local storage for unique session token, Responsive media queries, Keyframe animation",
         image: EntertainmentIQ,
         site_link: "https://pages.git.generalassemb.ly/jngong/entertainment-iq-trivia/",
@@ -17,7 +17,7 @@ export const projectData = [
     {
         title: "Gender Diversity in Film",
         tech_stack: "React, CSS, The Movie Database API, Bechdel Test API",
-        overview: "When it comes to gender parity in entertainment, Hollywood has a long way to go. Explore gender representation in the film industry by viewing gender breakdown percentages for the cast / crew and Bechdel Test results where available. This web app was built in one week to demonstrate full understanding of the React.js framework.",
+        overview: "When it comes to gender parity in entertainment, Hollywood has a long way to go. Explore the diversity of films based on results of the Bechdel Test and breakdown of gender % in the cast and crew. Search by film title to get started.",
         features: "User search for film title, Default home page view of trending movies, Dynamic routing to film detail pages, Calculation of cast / crew gender %, Match data from 2 APIs based on common identifier, Conditional rendering of Bechdel Test results, Responsive media queries",
         image: GenderDiversity,
         site_link: "https://gender-diversity-in-film.surge.sh/",
@@ -26,7 +26,7 @@ export const projectData = [
     {
         title: "Travelogue",
         tech_stack: "React, CSS, Ruby on Rails, PostgreSQL, bcrypt, JWT, Heroku",
-        overview: "Ever been asked for travel recommendations and come up at a loss for what you enjoyed on that trip 3 years ago? Travelogue helps you keep track of where you've been so you can help others get to where they're going. This full-stack web app was built in one week as my final capstone project for my Software Engineering Immersive course.",
+        overview: "Ever been asked for travel recommendations and come up at a loss for what you enjoyed on that trip 3 years ago? Travelogue helps you keep track of where you've been so you can help others get to where they're going. I designed, built and deployed the MVP for this full-stack web application in one week as the final project for my Software Engineering Immersive course.",
         features: "User sign up / login with encrypted password & JWT authorization, User can create, read, update & delete their own entries, Public view of places filterable by country, Model-View-Controller (MVC) design pattern, REST API backend, Responsive media queries",
         image: Travelogue,
         site_link: "https://travelogue-app.herokuapp.com/",
@@ -35,7 +35,7 @@ export const projectData = [
     {
         title: "Expedia",
         tech_stack: "React, CSS, Node.js, Express.js, Sequelize, PostgreSQL, Heroku",
-        overview: "As consumers demand more sustainable options in their everyday lives, we've introduced a new feature for Expedia to highlight eco-friendly hotels. In collaboration with a team of UX designers and engineers, we built a full-stack Expedia clone in one week following agile / scrum methodology and using React, CSS, Node.js, Express.js, Sequelize and PostgreSQL.",
+        overview: "As consumers demand more sustainable options in their everyday lives, we've introduced a new feature for Expedia to highlight eco-friendly hotels. In collaboration with a team of UX designers and engineers, I built a full-stack Expedia clone in one week following agile methodology and scrum framework.",
         features: "Mobile first design, User search for bundled vacation packages, Checkbox for eco-friendly options only, User flow to select hotels and room types, Pixel perfect CSS matching UX mock-ups in Zeplin, Admin section for listing, updating or deleting a new hotel",
         image: Expedia,
         site_link: "https://expedia-flying-four.herokuapp.com/",
@@ -44,7 +44,7 @@ export const projectData = [
     {
         title: "Friends of Columbus Park",
         tech_stack: "HTML, CSS, jQuery, Fancybox, Slick Carousel",
-        overview: "Redesigned website for a local Chinatown, NY based not-for-profit organization focused on protecting and advocating for Columbus Park and its surrounding community members. This was the first end-to-end project that I built as a developer and was the final project for my part-time Front End Web Development course.",
+        overview: "Redesigned website for a local Chinatown, NY based not-for-profit organization, focused on protecting and advocating for Columbus Park and its surrounding community members. This was the first end-to-end project that I built as a developer and was the final project for my part-time Front End Web Development course.",
         features: "Sticky navigation bar, Mobile responsiveness incl. collapsable menu, Integration of jQuery libraries for image carousel and lightbox functionality, Heavy use of flexbox for layout",
         image: Focp,
         site_link: "https://jngong.github.io/focp-ny-website/",
